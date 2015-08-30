39d8c6743296e93bd2d54221718b36d7b199c648
masterhilli <martin.hillbrand@gmail.com>
2015-08-30 11:52:46 +0200

    added method to resize any given image to a image with the size 200x200

--- ResizeImages/src/com/hilli/image/manipulation/ImageResizer.java
+++ ResizeImages/src/com/hilli/image/manipulation/ImageResizer.java
@@ -1,5 +1,72 @@
 package com.hilli.image.manipulation;
 
-public class ImageResizer {
+import java.awt.AlphaComposite;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+
+public class ImageResizer {
+	private static final int IMG_WIDTH = 400;
+	private static final int IMG_HEIGHT = 400;
+
+	private static final String JPG = "jpg";
+	private static final String PNG = "png";
+
+	public static boolean createImagesOutOfOriginalPath(String path, String outputPath) {
+		try {
+
+			File imageToRead = new File(path);
+			String fileName = imageToRead.getName();
+			BufferedImage originalImage = ImageIO.read(imageToRead);
+
+			createImageByType(outputPath + fileName, originalImage, JPG, true);
+			createImageByType(outputPath + fileName, originalImage, JPG, false);
+
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+			return false;
+		}
+		return true;
+
+	}
+
+	private static void createImageByType(String outputFilePath, BufferedImage originalImage, String type, boolean withHint)
+			throws IOException {
+		BufferedImage resizeImage = null;
+		int origtype = originalImage.getType() == 0 ? BufferedImage.TYPE_INT_ARGB : originalImage.getType();
+		if (withHint) {
+			resizeImage = resizeImageWithHint(originalImage, origtype);
+		} else {
+			resizeImage = resizeImage(originalImage, origtype);
+		}
+		ImageIO.write(resizeImage, type, new File(outputFilePath + (withHint ? "_hint." : ".") + type));
+	}
+
+	private static BufferedImage resizeImage(BufferedImage originalImage, int type) {
+		BufferedImage resizedImage = new BufferedImage(IMG_WIDTH, IMG_HEIGHT, type);
+		Graphics2D g = resizedImage.createGraphics();
+		g.drawImage(originalImage, 0, 0, IMG_WIDTH, IMG_HEIGHT, null);
+		g.dispose();
+
+		return resizedImage;
+	}
+
+	private static BufferedImage resizeImageWithHint(BufferedImage originalImage, int type) {
+
+		BufferedImage resizedImage = new BufferedImage(IMG_WIDTH, IMG_HEIGHT, type);
+		Graphics2D g = resizedImage.createGraphics();
+		g.drawImage(originalImage, 0, 0, IMG_WIDTH, IMG_HEIGHT, null);
+		g.dispose();
+		g.setComposite(AlphaComposite.Src);
+
+		g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+		g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+		return resizedImage;
+	}
 }
--- ResizeImages/src/tst/com/hilli/image/manipulation/ImageResizerTest.java
+++ ResizeImages/src/tst/com/hilli/image/manipulation/ImageResizerTest.java
@@ -1,14 +1,16 @@
 package tst.com.hilli.image.manipulation;
 
 import static org.junit.Assert.*;
-
+import com.hilli.image.manipulation.ImageResizer;
 import org.junit.Test;
 
 public class ImageResizerTest {
+	private static String TEST_FOLDER_PATH = "D:\\dev\\2015_JavaTryouts\\2015-Java-Tryouts\\ResizeImages\\tst\\";
+	private static String TEST_FOLDER_OUTPUT_PATH = TEST_FOLDER_PATH + "output\\";
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCreateImagesOutOfOriginalPath_InputCorrectPath_ReturnsNoException() {
+		assertTrue(ImageResizer.createImagesOutOfOriginalPath(TEST_FOLDER_PATH + "test1.jpg", TEST_FOLDER_OUTPUT_PATH));
 	}
 
 }
